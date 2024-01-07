86a45ed8f4803cece38e32c9c0274d29966b3cbd
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2024-01-07 18:19:11 -0300

    chore: adding properties to the component

--- exercicios/src/pages/basicos/usandoTitulo.jsx
+++ exercicios/src/pages/basicos/usandoTitulo.jsx
@@ -3,7 +3,19 @@ import Titulo from "../../../components/Titulo";
 export default function usandoTitulo() {
   return (
     <div>
-      <Titulo />
+      <Titulo
+        principal="Pagina de Cadastro"
+        secundario="Incluir, alterar e execluir cadastro"
+      />
+      <Titulo
+        principal="Pagina de Login"
+        secundario="Inform seu email e senha"
+        pequeno={true}
+      />
+      <Titulo
+        principal="Pagina de Cadastro"
+        secundario="Incluir, alterar e execluir cadastro"
+      />
     </div>
   );
 }
