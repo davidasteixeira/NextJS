fb81eda6ac6115b160f3e697dabfe6a0594af5ac
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2024-01-11 22:43:42 -0300

    feat: communication between father and son

---
+++ exercicios/src/pages/comunicacao/direta.jsx
@@ -0,0 +1,9 @@
+import Pai from "../../../components/direta/pai";
+
+export default function direta(){
+    return (
+        <div>
+            <Pai familia="Albuquerque #2" nome="Padrão" />
+        </div>
+    )
+}
