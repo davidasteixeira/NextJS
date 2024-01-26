e877a2a3d42c875108b55bf90ce82caf952fcd43
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2024-01-26 20:45:50 -0300

    feat: design port

--- portas/components/Porta.jsx
+++ portas/components/Porta.jsx
@@ -2,8 +2,14 @@ import style from "../src/styles/Porta.module.css";
 
 export default function Porta() {
   return (
-    <div className={style.porta}>
-      <div className={style.numero}>3</div>
+    <div className={style.area}>
+      <div className={style.estrutura}>
+        <div className={style.porta}>
+          <div className={style.numero}>3</div>
+          <div className={style.macaneta}></div>
+        </div>
+      </div>
+      <div className={style.chao}></div>
     </div>
   );
 }
