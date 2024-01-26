aad975c0e01f3c43287857246f77c98ff677ac2b
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2024-01-25 23:50:12 -0300

    feat: component port

---
+++ portas/components/Porta.jsx
@@ -0,0 +1,9 @@
+import style from "../src/styles/Porta.module.css";
+
+export default function Porta() {
+  return (
+    <div className={style.porta}>
+      <div className={style.numero}>3</div>
+    </div>
+  );
+}
--- portas/src/pages/index.jsx
+++ portas/src/pages/index.jsx
@@ -1,10 +1,9 @@
-import Presente from "../../components/Presente";
+import Porta from "../../components/Porta";
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
-      <Presente />
+      <Porta />
     </div>
   );
 }
