22ed2e2a416635d2ed2d5aa4ada4ed228fb1214a
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2024-01-26 20:55:50 -0300

    feat: add class selected and props in port

--- portas/components/Porta.jsx
+++ portas/components/Porta.jsx
@@ -1,9 +1,11 @@
 import style from "../src/styles/Porta.module.css";
 
-export default function Porta() {
+export default function Porta(props) {
+  const selecionada = props.selecionada ? style.selecionada : "";
+
   return (
     <div className={style.area}>
-      <div className={style.estrutura}>
+      <div className={`${style.estrutura} ${selecionada}`}>
         <div className={style.porta}>
           <div className={style.numero}>3</div>
           <div className={style.macaneta}></div>
