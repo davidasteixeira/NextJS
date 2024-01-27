7dbedb9258007426a96fa1e4c02d43545743ddd8
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2024-01-27 19:51:58 -0300

    feat: integration Typescript and add types e props for component

--- portas/components/Porta.tsx
+++ portas/components/Porta.tsx
@@ -1,13 +1,19 @@
 import style from "../src/styles/Porta.module.css";
+import PortaModel from "../model/porta";
 
-export default function Porta(props) {
-  const selecionada = props.selecionada ? style.selecionada : "";
+interface PortaProps {
+  porta: PortaModel;
+}
+
+export default function Porta(props: PortaProps) {
+  const { porta } = props;
+  const selecionada = porta.selecionada ? style.selecionada : "";
 
   return (
     <div className={style.area}>
       <div className={`${style.estrutura} ${selecionada}`}>
         <div className={style.porta}>
-          <div className={style.numero}>3</div>
+          <div className={style.numero}>{porta.numero}</div>
           <div className={style.macaneta}></div>
         </div>
       </div>
