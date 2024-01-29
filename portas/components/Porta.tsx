b902641742b5a482671947efbb7b431f5fd46e3e
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2024-01-28 21:50:32 -0300

    feat: new events ports

--- portas/components/Porta.tsx
+++ portas/components/Porta.tsx
@@ -8,17 +8,28 @@ interface PortaProps {
 
 export default function Porta(props: PortaProps) {
   const porta = props.value;
-  const selecionada = porta.selecionada ? style.selecionada : "";
+  const selecionada =
+    porta.selecionada && !porta.aberta ? style.selecionada : "";
 
   const alternarSelececao = (e) => props.onChange(porta.alternarSelecao());
+  const abrir = (e) => {
+    e.stopPropagation();
+    props.onChange(porta.abrir());
+  };
+
+  function renderizarPorta() {
+    return (
+      <div className={style.porta}>
+        <div className={style.numero}>{porta.numero}</div>
+        <div className={style.macaneta} onClick={abrir}></div>
+      </div>
+    );
+  }
 
   return (
     <div className={style.area} onClick={alternarSelececao}>
       <div className={`${style.estrutura} ${selecionada}`}>
-        <div className={style.porta}>
-          <div className={style.numero}>{porta.numero}</div>
-          <div className={style.macaneta}></div>
-        </div>
+        {porta.aberta ? false : renderizarPorta()}
       </div>
       <div className={style.chao}></div>
     </div>
