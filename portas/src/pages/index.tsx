67948d4f6218db6f8c78c6651cced99baf52818c
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2024-01-31 23:29:48 -0300

    feat: create page game

--- portas/src/pages/index.tsx
+++ portas/src/pages/index.tsx
@@ -1,26 +1,7 @@
-import { useState } from "react";
-import Porta from "../../components/Porta";
-import PortaModel from "../../model/porta";
-import { atualizarPortas, criarPortas } from "../../functions/portas";
-
 export default function Home() {
-  const [p1, setP1] = useState(new PortaModel(1));
-
-  const [portas, setPortas] = useState(criarPortas(3, 2));
-
-  function renderizarPortas() {
-    return portas.map((porta) => {
-      return (
-        <Porta
-          value={porta}
-          key={porta.numero}
-          onChange={(novaPorta) =>
-            setPortas(atualizarPortas(portas, novaPorta))
-          }
-        />
-      );
-    });
-  }
-
-  return <div style={{ display: "flex" }}>{renderizarPortas()}</div>;
+  return (
+    <div>
+      <h1>Formulário de inicio</h1>
+    </div>
+  );
 }
