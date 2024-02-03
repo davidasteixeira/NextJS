52d8277b4db3c3e0d57ec652a3a0bb51a09b0d2d
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2024-02-02 23:24:58 -0300

    feat: estrutere of form page initial

--- portas/src/pages/index.tsx
+++ portas/src/pages/index.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
+import Cartao from "../../components/Cartao";
+import styles from "../styles/Formulario.module.css";
+import Link from "next/link";
+
+export default function Formulario() {
   return (
-    <div>
-      <h1>Formulário de inicio</h1>
+    <div className={styles.formulario}>
+      <div>
+        <Cartao bgColor="#c0392c">
+          <h1>Monty Hall</h1>
+        </Cartao>
+        <Cartao></Cartao>
+      </div>
+      <div>
+        <Cartao></Cartao>
+        <Cartao bgColor="#28a085">
+          <Link href={`/jogo/4/2`} className={styles.link}>
+            <h2>Iniciar</h2>
+          </Link>
+        </Cartao>
+      </div>
     </div>
   );
 }
