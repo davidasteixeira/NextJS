51c08fd99f69a7e4587c490c51bce4b8ebcec03d
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2024-02-16 21:07:18 -0300

    feat: getStaticPaths and fallback

---
+++ renderizacao/src/pages/alunos/[id].jsx
@@ -0,0 +1,46 @@
+export async function getStaticPaths() {
+  const resp = await fetch("http://localhost:3000/api/alunos/tutores");
+
+  const ids = await resp.json();
+
+  const paths = ids.map((id) => {
+    return { params: { id: `${id}` } };
+  });
+
+  return {
+    fallback: true,
+    paths,
+  };
+}
+
+export async function getStaticProps(context) {
+  const resp = await fetch(
+    `http://localhost:3000/api/alunos/${context.params.id}`
+  );
+  const aluno = await resp.json();
+
+  return {
+    props: {
+      aluno,
+    },
+  };
+}
+
+export default function AlunoPorId(props) {
+  const { aluno } = props;
+  console.log(aluno);
+  return (
+    <div>
+      <h1>Detalhes do Aluno</h1>
+      {aluno ? (
+        <ul>
+          <li>{aluno.id}</li>
+          <li>{aluno.nome}</li>
+          <li>{aluno.email}</li>
+        </ul>
+      ) : (
+        false
+      )}
+    </div>
+  );
+}
