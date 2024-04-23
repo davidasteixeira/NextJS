e94efae493e3a28422362bb00108415bbe943f45
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2024-04-23 20:06:15 -0300

    feat: created button for question component

--- quiz/src/pages/index.tsx
+++ quiz/src/pages/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Questao from "../../components/Questao";
 import QuestaoModel from "../../model/questao";
 import RespostaModel from "../../model/resposta";
+import Botao from "../../components/Botao";
 
 const questaoMock = new QuestaoModel(1, "Melhor cor?", [
   RespostaModel.errada("Verde"),
@@ -29,15 +30,18 @@ export default function Home() {
       style={{
         display: "flex",
         justifyContent: "center",
+        flexDirection: "column",
         alignItems: "center",
         height: "100vh",
       }}
     >
       <Questao
         valor={questao}
+        tempoPraResposta={5}
         respostaFornecida={respostaFornecida}
         tempoEsgotado={tempoEsgotado}
       />
+      <Botao texto="Proxíma" href="/resultado"></Botao>
     </div>
   );
 }
