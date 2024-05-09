889ec7d3b055abdfb95afa46b8f1ccb870db5a54
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2024-05-09 10:37:17 -0300

    feat: adjust component menuItem and created link for notifications and configs

--- admin-template/src/components/templates/MenuItem.tsx
+++ admin-template/src/components/templates/MenuItem.tsx
@@ -1,8 +1,29 @@
+import Link from "next/link";
+
 interface MenuItemProps {
   url: string;
   texto: string;
   icone: any;
 }
-export default function MenuItem(props) {
-  return <li className={``}>{props.icone}</li>;
+export default function MenuItem(props: MenuItemProps) {
+  return (
+    <li
+      className={`hover:bg-gray-100
+  `}
+    >
+      <Link href={props.url}>
+        <div
+          className={`
+            flex flex-col justify-center items-center
+            w-20 h-20
+        `}
+        >
+          {props.icone}
+          <span className={`text-xs font-ligth text-gray-600`}>
+            {props.texto}
+          </span>
+        </div>
+      </Link>
+    </li>
+  );
 }
---
+++ admin-template/src/pages/ajustes.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/templates/Layout";
+
+export default function Ajustes() {
+  return (
+    <Layout titulo="Ajustes" subtitulo="Aqui você irá realizar ajustes">
+      <h3>Conteudo</h3>
+    </Layout>
+  );
+}
