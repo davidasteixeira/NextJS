186883fc783373f4e97fac81749012a237ca75a7
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2024-05-09 09:52:24 -0300

    feat: initialized project

---
+++ admin-template/src/components/templates/Layout.tsx
@@ -0,0 +1,24 @@
+import Cabecalho from "./Cabecalho";
+import MenuLateral from "./MenuLatarel";
+import Conteudo from "./Conteudo";
+
+interface LayoutProps {
+  titulo: string;
+  subtitulo: string;
+  children?: any;
+}
+export default function Layout(props: LayoutProps) {
+  return (
+    <div className={`flex h-screen w-screen`}>
+      <MenuLateral />
+      <div
+        className={`flex flex-col w-full p-7
+        bg-gray-300
+        `}
+      >
+        <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo} />
+        <Conteudo>{props.children}</Conteudo>
+      </div>
+    </div>
+  );
+}
---
+++ admin-template/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/templates/Layout";
+
+export default function Home() {
+  return (
+    <Layout titulo="Pagina Inicial" subtitulo="Estamos construindo um templete">
+      <h3>Conteudo</h3>
+    </Layout>
+  );
+}
