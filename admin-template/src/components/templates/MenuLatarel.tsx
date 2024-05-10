f2d7f4d4b08add2d7eaa12a2536c8e4c9f03b0b9
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2024-05-09 21:45:11 -0300

    feat: add component logo

--- admin-template/src/components/templates/MenuLatarel.tsx
+++ admin-template/src/components/templates/MenuLatarel.tsx
@@ -1,9 +1,16 @@
 import MenuItem from "./MenuItem";
 import { IconeAjustes, IconeCasa, IconeSino } from "../icons";
+import Logo from "./Logo";
 
 export default function MenuLateral() {
   return (
     <aside>
+      <div
+        className={`flex flex-col items-center justify-center
+          w-20 h-20 bg-gradient-to-r from-indigo-500 to-purple-800`}
+      >
+        <Logo></Logo>
+      </div>
       <ul>
         <MenuItem url="/" texto="Inicio" icone={IconeCasa} />
         <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
