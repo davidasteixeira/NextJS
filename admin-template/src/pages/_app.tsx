466f519f981db1c49ceb59d97d9f1bf6e32e1dc1
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2024-05-13 20:13:21 -0300

    feat: added context for auth google

--- admin-template/src/pages/_app.tsx
+++ admin-template/src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "../styles/global.css";
 import "tailwindcss/tailwind.css";
 import { AppProvider } from "../data/context/AppContext";
+import { AuthProvider } from "../data/context/AuthContext";
 
 export default function App({ Component, pageProps }) {
   return (
-    <AppProvider>
-      <Component {...pageProps} />
-    </AppProvider>
+    <AuthProvider>
+      <AppProvider>
+        <Component {...pageProps} />
+      </AppProvider>
+    </AuthProvider>
   );
 }
