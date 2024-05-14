cecce945498917431f7c87525a4e77048ebe5737
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2024-05-14 10:09:06 -0300

    feat: added component avatar for perfil and page for perfil

---
+++ admin-template/src/pages/perfil.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/templates/Layout";
+
+export default function Perfil() {
+  return (
+    <Layout titulo="Perfil" subtitulo="Aqui você irá gerenciar seu Perfil">
+      <h1>Perfil</h1>
+    </Layout>
+  );
+}
